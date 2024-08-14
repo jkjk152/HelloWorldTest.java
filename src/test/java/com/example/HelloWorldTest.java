00d7b55ff5f845e2e0d706c8cf4954789dd3e779
jkjk152 <jkjkjk152@gmail.com>
2024-08-14 10:55:17 +0800

    first commit

--- src/test/java/com/example/HelloWorldTest.java
+++ src/test/java/com/example/HelloWorldTest.java
@@ -6,6 +6,6 @@ public class HelloWorldTest {
     @Test
     public void testGetGreeting() {
         HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello, World!nnn", helloWorld.getGreeting());
+        assertEquals("Hello, World!", helloWorld.getGreeting());
     }
 }
